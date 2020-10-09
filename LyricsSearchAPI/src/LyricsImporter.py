2d9d9f4a28902e7e7c58256451d9d68a4f28e72c
WaylonLuo <WaylonLuo@192.168.0.2>
2020-10-08 23:53:22 -0400

    LyricsImporter commit

---
+++ LyricsSearchAPI/src/LyricsImporter.py
@@ -0,0 +1,44 @@
+import pymongo
+import json
+from datetime import datetime
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient['LyricsDB']
+myTbl = mydb['Lyrics']
+
+# filepath = 'archived_tweets/nys_tweets_filtered_2014_Q2.json'
+filepath = 'archived_tweets/lyrics.json'
+
+count = 0; 
+with open(filepath, 'r') as f:
+    data = json.load(f)
+    for d in data: 
+#         print(d)
+
+        try:
+            name = str(d['name'])
+            title = d['title']
+            year = d['year']
+            genius_songID = d['genius_songID']
+            x_add = d['x_add']
+            lyrics4 = d['lyrics4'].replace('\n\n\n', '\n')
+              
+            data = {'name': name ,
+                    'title': title ,
+                    'year': year ,
+                    'genius_songID': genius_songID,
+                    'x_add': x_add, 
+                    'lyrics4': lyrics4}
+            print(data)
+            
+        except:
+            print('empty json') 
+        try:
+            myTbl.insert_one(data)
+        except:
+            print('insertion error')
+    
+# with open('archived_tweets/log.txt', 'w') as logfile:
+#     logfile.write('finished importing file: {} at {}'.format(filepath, datetime.now().time())) 
+        
